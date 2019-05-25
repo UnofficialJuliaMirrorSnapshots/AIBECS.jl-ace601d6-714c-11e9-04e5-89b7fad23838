d665507fd05a570247b1b843b4431f367ba035b1
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-25 05:46:31 +0000

    Snapshot of branch master taken on 2019-05-25T01:46:30.288-04:00 from "https://github.com/UnofficialJuliaMirror/AIBECS.jl-ace601d6-714c-11e9-04e5-89b7fad23838"

--- docs/generate.jl
+++ docs/generate.jl
@@ -12,22 +12,10 @@ for example in readdir(EXAMPLEDIR)
     script = Literate.script(input, GENERATEDDIR)
     code = strip(read(script, String))
     mdpost(str) = replace(str, "@__CODE__" => code)
-    Literate.markdown(input, GENERATEDDIR, postprocess = mdpost)
     Literate.notebook(input, GENERATEDDIR, execute = true)
+    Literate.markdown(input, GENERATEDDIR, postprocess = mdpost)
     # Altenrate comments to avoid running the code when editing the text only!
     #Literate.notebook(input, GENERATEDDIR, execute = false)
-    #Literate.markdown(input, GENERATEDDIR, documenter=false)
+    #Literate.markdown(input, GENERATEDDIR, documenter = false)
 end
 
-# copy some figures to the build directory (no figures for me right now)
-# cp(joinpath(@__DIR__, "../examples/figures/heat_square.png"),
-#    joinpath(@__DIR__, "src/examples/generated/heat_equation.png"); force = true)
-# 
-# cp(joinpath(@__DIR__, "../examples/figures/coloring.png"),
-#    joinpath(@__DIR__, "src/examples/generated/coloring.png"); force = true)
-
-# remove any .vtu files in the generated dir (should not be deployed)
-# I don't even know what VTU files are... Leaving this here for now.
-cd(GENERATEDDIR) do
-    foreach(file -> endswith(file, ".vtu") && rm(file), readdir())
-end
